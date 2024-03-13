baa1282b43ef0823f2227a8649bafcffa5636d42
otoumas <tomas@horizenlabs.io>
2024-03-13 16:45:52 +0000

    Improved documentation in FORGER.md and scripts

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -32,7 +32,7 @@ ENV_FILE_TEMPLATE="${ROOT_DIR}/env/.env.${role_value}.${network_value}.template"
 ENV_FILE="${DEPLOYMENT_DIR}/.env"
 
 # Create a backup of .env file called .env.bk
-echo -e "\n\033[1m=== Creating a backup of the .env file ===\033[0m"
+echo -e "\n\033[1m=== Creating a backup of the ${ENV_FILE} file ===\033[0m"
 cp "${ENV_FILE}" "${ENV_FILE}.bk"
 
 # Define the auto update variables
@@ -50,30 +50,30 @@ conditional_update_vars=(
   "SCNODE_LOG_FILE_LEVEL"
 )
 
-# Read the .env.template file line by line, skip blanc lines and comments, store each of the other lines in an array
-echo -e "\n\033[1m=== Reading .env.template file ===\033[0m"
+# Read the .env.template file line by line, skip blank lines and comments, store each of the other lines in an array
+echo -e "\n\033[1m=== Reading ${ENV_FILE_TEMPLATE} file ===\033[0m"
 while IFS= read -r line; do
-  [ -z "$line" ] && continue
+  [ -z "${line}" ] && continue
   [ "${line:0:1}" = "#" ] && continue
-  env_template_lines+=("$line")
-done <"$ENV_FILE_TEMPLATE"
+  env_template_lines+=("${line}")
+done <"${ENV_FILE_TEMPLATE}"
 
 # Append new env vars to .env file
-echo -e "\n\033[1m=== Appending new env vars to .env file ===\033[0m"
+echo -e "\n\033[1m=== Appending new env vars to ${ENV_FILE} file ===\033[0m"
 for line in "${env_template_lines[@]}"; do
-  var_name=$(echo "$line" | cut -d'=' -f1)
-  if ! grep -q "^$var_name=" "$ENV_FILE"; then
-    echo "$line" >>"$ENV_FILE"
+  var_name=$(echo "${line}" | cut -d'=' -f1)
+  if ! grep -q "^${var_name}=" "${ENV_FILE}"; then
+    echo -e "\n${line}" >>"${ENV_FILE}"
   fi
 done
 
 # Update the values of the auto update variables
 echo -e "\n\033[1m=== Updating the values of the auto update variables ===\033[0m"
 for line in "${env_template_lines[@]}"; do
-  var_name=$(echo "$line" | cut -d'=' -f1)
+  var_name=$(echo "${line}" | cut -d'=' -f1)
   for item in "${auto_update_vars[@]}"; do
-    if [[ "$item" == "$var_name" ]]; then
-      sed -i "/^$var_name=/c\\$line" "$ENV_FILE"
+    if [[ "${item}" == "${var_name}" ]]; then
+      sed -i "/^${var_name}=/c\\${line}" "${ENV_FILE}"
       break
     fi
   done
@@ -82,20 +82,20 @@ done
 # Update the values of the conditional update variables if approved by the user
 echo -e "\n\033[1m=== Updating the values of the conditional update variables ===\033[0m"
 for line in "${env_template_lines[@]}"; do
-  var_name=$(echo "$line" | cut -d'=' -f1)
+  var_name=$(echo "${line}" | cut -d'=' -f1)
   for item in "${conditional_update_vars[@]}"; do
-    if [[ "$item" == "$var_name" ]]; then
-      if ! grep -q "^$line" "$ENV_FILE"; then
-        echo -e "\nThe value of $var_name in the .env file is different from the value in the .env.template file."
-        echo -e "Current value: \033[1m$(grep "^$var_name=" "$ENV_FILE")\033[0m"
-        echo -e "New value: \033[1m$line\033[0m\n"
-        read -rp "Would you like to update the value of $var_name in the .env file to the value from the .env.template file? ('yes' or 'no') " answer
+    if [[ "${item}" == "${var_name}" ]]; then
+      if ! grep -q "^${line}" "${ENV_FILE}"; then
+        echo -e "\nThe value of ${var_name} in the ${ENV_FILE} file is different from the value in the ${ENV_FILE_TEMPLATE} file."
+        echo -e "${ENV_FILE} value: \033[1m$(grep "^${var_name}=" "${ENV_FILE}")\033[0m"
+        echo -e "${ENV_FILE_TEMPLATE} value: \033[1m${line}\033[0m\n"
+        read -rp "Would you like to update the value of ${var_name} in the ${ENV_FILE} file to the value from the ${ENV_FILE_TEMPLATE} file? ('yes' or 'no') " answer
         while [[ ! "${answer}" =~ ^(yes|no)$ ]]; do
           echo -e "\nError: The only allowed answers are 'yes' or 'no'. Try again...\n"
-          read -rp "Would you like to update the value of $var_name in the .env file to the value from the .env.template file? ('yes' or 'no') " answer
+          read -rp "Would you like to update the value of ${var_name} in the ${ENV_FILE} file to the value from the ${ENV_FILE_TEMPLATE} file? ('yes' or 'no') " answer
         done
-        if [ "$answer" = "yes" ]; then
-          sed -i "/^$var_name=/c\\$line" "$ENV_FILE"
+        if [ "${answer}" = "yes" ]; then
+          sed -i "/^${var_name}=/c\\${line}" "${ENV_FILE}"
         fi
       fi
       break
@@ -103,10 +103,14 @@ for line in "${env_template_lines[@]}"; do
   done
 done
 
-echo ".env upgrade completed successfully"
+echo -e "\n\033[1m=== ${ENV_FILE} upgrade completed successfully ==="
 
-echo "Please review the changes in the .env file, if there is anything wrong you can restore the backup file .env.bk"
+echo -e "\n=== Please review the changes in the ${ENV_FILE} file, if there is anything wrong you can restore the backup file ${ENV_FILE}.bk ==="
 
-echo "Run cd deployments/${role_value}/${network_value} && docker-compose down && docker-compose up -d to apply the changes."
+echo -e "\n=== In order to apply the changes, run: "
+
+echo -e "\ndocker compose -f ${ROOT_DIR}/deployments/${role_value}/${network_value}/docker-compose.yml down && docker compose -f ${ROOT_DIR}/deployments/${role_value}/${network_value}/docker-compose.yml up -d "
+
+echo -e "\n===\033[0m"
 
 exit 0
