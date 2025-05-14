e3c4ad008c3cbbb5691fdae6a2a824730d4719e2
paolocappelletti <paolo@horizenlabs.io>
2025-05-14 15:55:49 +0200

    updates

--- scripts/init.sh
+++ scripts/init.sh
@@ -200,6 +200,8 @@ if [[ "${role_value}" =~ ^(forger|migration)$ ]]; then
   
   echo -e "\n\033[1m=== RUNNING ${role_value} NODE ===\033[0m\n"
 
+  echo -e "0. (Optional Step) Download the ZEND seed into ${DEPLOYMENT_DIR}/${role_value}/${network_value}/seed/ folder\n"
+
   echo -e "1. First, run the zend node:"
 
   echo -e "\n\033[1mdocker compose -f ${DEPLOYMENT_DIR}/docker-compose.yml up -d zend\033[0m\n"
